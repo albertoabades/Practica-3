54cb4815b4e9acf927d603c2f4bcafa563255456
albertoabades <alberto.abades@gmail.com>
2014-10-28 22:59:35 +0100

    Disparo cada vez que pulso, game.js modificado

--- 08-missiles/game.js
+++ 08-missiles/game.js
@@ -100,6 +100,7 @@ var PlayerShip = function() {
     this.x = Game.width/2 - this.w / 2;
     this.y = Game.height - 10 - this.h;
     this.vx = 0;
+	this.disparo=false;
 
     this.reloadTime = 0.25;  // Un cuarto de segundo para poder volver a disparar
     this.reload = this.reloadTime;
@@ -118,16 +119,19 @@ var PlayerShip = function() {
 	    this.x = Game.width - this.w 
 	}
 
-	this.reload-=dt;
-	if(Game.keys['fire'] && this.reload < 0) {
+	//this.reload-=dt;
+	//if(Game.keys['fire'] && this.reload < 0) {
+	if(Game.keys['fire'] && !this.disparo) {
 	    // Esta pulsada la tecla de disparo y ya ha pasado el tiempo reload
-	    Game.keys['fire'] = false;
-	    this.reload = this.reloadTime;
+	    //Game.keys['fire'] = false;
+		this.disparo=true;
+	    //this.reload = this.reloadTime;
 
 	    // Se añaden al gameboard 2 misiles 
 	    this.board.add(new PlayerMissile(this.x,this.y+this.h/2));
 	    this.board.add(new PlayerMissile(this.x+this.w,this.y+this.h/2));
 	}
+	if(!Game.keys['fire']){this.disparo=false;}
     }
 
     this.draw = function(ctx) {
